d4f1058879e3ca2112abf8b3eb9b3b4618dffab2
AntonChm <fandr@ukr.net>
2020-12-08 18:49:43 +0200

    test

---
+++ script.js
@@ -0,0 +1,11 @@
+const messanges = [
+  {autor:"John",
+text:"dfjkghdfgjkhdfgjkdgjhfdgjk",
+income: false,},
+{autor:"John",
+text:"dfjkghdfgjkhdfgjkdgjhfdgjk",
+income: true,},
+{autor:"John",
+text:"dfjkghdfgjkhdfgjkdgjhfdgjk",
+income: false,},
+];
